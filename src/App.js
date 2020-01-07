7fb4831868d3f00e89e5aa3fcc2b099c4a8fa4e2
Natalie <49326857+natyeo@users.noreply.github.com>
2020-01-07 14:27:41 +0000

    Created first component - welcome message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/welcome.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <body>
+        <div>
+          <Welcome name="Sara" />
+          </div>
+      </body>
     </div>
   );
 }
