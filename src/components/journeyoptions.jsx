58cfbd3390a07fcb066199fe3cb597ddfc74e58c
Sara <srancati0@gmail.com>
2020-01-09 17:23:23 +0000

    Updated journeyoptions to open link in new tab
    Co-Authored-By: Elfi Yang <elfiyang16@users.noreply.github.com>
    Co-Authored-By: Freddie Smith Hughes <sevensecrets@users.noreply.github.com>

--- src/components/journeyoptions.jsx
+++ src/components/journeyoptions.jsx
@@ -8,7 +8,7 @@ class JourneyOptions extends React.Component {
             <td>{this.props.results.travel_time}</td>
             <td>{this.props.results.distance}</td>
             <td>{this.props.results.carbon}</td>
-            <td><a id={this.props.results.mode} href={"https://www.google.com/maps/dir/?api=1&origin=" + this.props.from + "&destination=" + this.props.to + "&travelmode=" + this.props.results.mode}>Select</a></td>
+            <td><a id={this.props.results.mode} target="_blank" href={"https://www.google.com/maps/dir/?api=1&origin=" + this.props.from + "&destination=" + this.props.to + "&travelmode=" + this.props.results.mode}>Open with Google Maps</a></td>
         </tr>
     );
   }
