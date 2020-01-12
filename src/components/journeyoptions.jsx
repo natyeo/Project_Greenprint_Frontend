a8fb0c37d85f5099af3de6818276f0c02bd829e8
elfi yang <elfiyang16@gmail.com>
2020-01-12 22:47:26 +0000

    feat: add button for user to save journey to database

--- src/components/journeyoptions.jsx
+++ src/components/journeyoptions.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from "react";
+import { userService } from "../services/authentication.service";
 
 class JourneyOptions extends React.Component {
+  saveJourneyToDB;
+
   render() {
     return (
       <tr>
@@ -8,7 +11,26 @@ class JourneyOptions extends React.Component {
         <td>{this.props.results.travel_time}</td>
         <td>{this.props.results.distance} miles.</td>
         <td>{this.props.results.carbon}kg of Carbon.</td>
-        <td><a id={this.props.results.mode} target="_blank" rel="noopener noreferrer" href={"https://www.google.com/maps/dir/?api=1&origin=" + this.props.from + "&destination=" + this.props.to + "&travelmode=" + this.props.results.mode}>Open with Google Maps</a></td>
+        <td>
+          <a
+            id={this.props.results.mode}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={
+              "https://www.google.com/maps/dir/?api=1&origin=" +
+              this.props.from +
+              "&destination=" +
+              this.props.to +
+              "&travelmode=" +
+              this.props.results.mode
+            }
+          >
+            Open with Google Maps
+          </a>
+        </td>
+        <td>
+          <button onClick={this.saveJourneyToDB}>Save journey</button>
+        </td>
       </tr>
     );
   }
