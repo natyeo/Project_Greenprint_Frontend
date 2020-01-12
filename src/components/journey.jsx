c9d25e0991ab6e3621149e6cbedd740f128e14fd
elfi yang <elfiyang16@gmail.com>
2020-01-12 19:05:00 +0000

    file: add dependencies

--- src/components/journey.jsx
+++ src/components/journey.jsx
@@ -1,81 +1,100 @@
-import React from 'react';
-import JourneyOptions from './journeyoptions';
-import './journey.css';
-import Modal from './Modal/Modal';
+import React from "react";
+import JourneyOptions from "./journeyoptions";
+import "./journey.css";
+import Modal from "./Modal/Modal";
 
 class JourneyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      from: '',
-      to: '',
+      from: "",
+      to: "",
       options: false,
       loading: false
     };
   }
 
-  handleChange = (event) => {
+  handleChange = event => {
     const name = event.target.name;
     const value = event.target.value;
 
     this.setState({
       [name]: value
     });
-  }
+  };
 
   apiCall() {
-    const data = {from: this.state.from , to: this.state.to}
+    const data = { from: this.state.from, to: this.state.to };
     // remember to change the route below for production
-    return fetch('https://cors-anywhere.herokuapp.com/https://project-greenprint-backend.herokuapp.com/', {
-      method: 'POST',
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify(data)
-    })
-      .then((data) =>  data.json())
-      .then((body) => {
+    return fetch(
+      "https://cors-anywhere.herokuapp.com/https://project-greenprint-backend.herokuapp.com/test-route",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(data)
+      }
+    )
+      .then(data => data.json())
+      .then(body => {
         this.setState({
           options: body.results,
           loading: false
         });
-        return body
-      })
-    }
-
-  handleSubmit = (event) => {
-    this.apiCall();
-    this.setState({loading: true})
-    event.preventDefault();
+        return body;
+      });
   }
 
+  handleSubmit = event => {
+    this.apiCall();
+    this.setState({ loading: true });
+    event.preventDefault();
+  };
+
   journeyOptionsList() {
-    const startPoint = this.state.from
-    const endPoint = this.state.to
-    return this.state.options.map(function(elem, i){
-      return <JourneyOptions results={elem} key={i} from={startPoint} to={endPoint}/>
-    })
+    const startPoint = this.state.from;
+    const endPoint = this.state.to;
+    return this.state.options.map(function(elem, i) {
+      return (
+        <JourneyOptions
+          results={elem}
+          key={i}
+          from={startPoint}
+          to={endPoint}
+        />
+      );
+    });
   }
 
   render() {
     return (
       <div>
-
         <div className="grouping">
           <h3>Journey Calculation</h3>
           <form onSubmit={this.handleSubmit} className="ui form">
             <div>
               <label>
                 <span className="bold">FROM</span>
-                <input name="from" type="text" value={this.state.from} onChange={this.handleChange} />
+                <input
+                  name="from"
+                  type="text"
+                  value={this.state.from}
+                  onChange={this.handleChange}
+                />
               </label>
             </div>
             <div>
               <label>
                 <span className="bold">TO</span>
-                <input name="to" type="text" value={this.state.to} onChange={this.handleChange} />
+                <input
+                  name="to"
+                  type="text"
+                  value={this.state.to}
+                  onChange={this.handleChange}
+                />
               </label>
             </div>
             <div>
@@ -84,25 +103,22 @@ class JourneyForm extends React.Component {
           </form>
         </div>
 
-        { this.state.options ?
-
+        {this.state.options ? (
           <div className="grouping results">
             <h3>Your travel results</h3>
-            <table className="ui celled striped table" style={{textAlign: "center"}} >
-              <tbody>
-                {this.journeyOptionsList() }
-              </tbody>
+            <table
+              className="ui celled striped table"
+              style={{ textAlign: "center" }}
+            >
+              <tbody>{this.journeyOptionsList()}</tbody>
             </table>
-          </div> :
+          </div>
+        ) : (
           <> </>
-        }
-        { this.state.loading ?
-               
-        <Modal /> :
-        <> </>
-      }
+        )}
+        {this.state.loading ? <Modal /> : <> </>}
       </div>
-    )
+    );
   }
 }
 
