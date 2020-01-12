c87669e05b1cf139b6a82ab858ea4b640c84dc51
Andy Jones <andytjones@icloud.com>
2020-01-12 19:15:20 +0000

    Feature - navbar component.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/welcome.jsx';
 import JourneyForm from './components/journey.jsx';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends React.Component {
 
   render(){
     return (
       <div className="App">
-          <Welcome name="there" />
-          <div className="journey-planner">
-            <JourneyForm />
-          </div>
+        <Navbar />
+        <Welcome name="there" />
+        <div className="journey-planner">
+          <JourneyForm />
+        </div>
       </div>
     );
   }
