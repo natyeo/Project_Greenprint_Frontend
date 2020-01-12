4381fc860995eeffa616ba06082b1c3c86596f4b
elfi yang <elfiyang16@gmail.com>
2020-01-12 19:32:40 +0000

    feat: add 1st input field in register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
 // import "./Login.css";
 
@@ -13,6 +13,32 @@ class Register extends Component {
       password: "",
       password2: ""
     };
+  }
+
+  render() {
+    const { username, email, password, password2 } = this.state;
+    return (
+      <div class="container">
+        <form onSubmit={this.onSubmit}>
+          <h2>Register</h2>
+
+          <label>Username</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="User Name"
+            name="username"
+            value={username}
+            onChange={this.onChange}
+            required
+          />
+
+          <button class="btn btn-lg btn-primary btn-block" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
