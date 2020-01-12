06c9b72dfc187f30820d803a89a60481bcb967fd
elfi yang <elfiyang16@gmail.com>
2020-01-12 19:17:18 +0000

    feat: initiate Register class component

---
+++ src/components/Register.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
+// import "./Login.css";
+
+class Register extends Component {}
+
+export default Register;
