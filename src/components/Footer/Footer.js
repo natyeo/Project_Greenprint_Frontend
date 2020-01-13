f6ee32b90bd052913f41080b61ef69c31e29c4b4
Andy Jones <andytjones@icloud.com>
2020-01-13 13:17:07 +0000

    Styling - Updated footer styling.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ const footer = () => {
           <p>This app uses calculates the carbon data based around the average stuff to go in here as we want to explain what the data is. Define the underlying principles that drive decisions and strategy for your design language. A better understanding of usage can aid in prioritizing future efforts. Out of scope. All hands on deck marketing computer development html roi feedback team website today shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud , but thinking outside the box proceduralize, for work. </p>
         </div>
       </div>
-      <div className="ui height grid tagline">
+      <div className="tagline">
           <p>Created with love Project GreenPrint <span>{(new Date().getFullYear())}</span>
           </p>
       </div>
