4adec0b4c84dc70da2c09fb02306ffd286556349
Andy Jones <andytjones@icloud.com>
2020-01-13 12:21:27 +0000

    Styling - Updated footer styling.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,24 @@ import './Footer.css';
 
 const footer = () => {
   return (
-    <div>
-      "This is the footer"
+    <div className="ui footer segment vertical">
+      <div className=" ui stackable height grid">
+        <div className="three wide column">
+          <h3>Resources</h3>
+          <ul>
+            <li>Google Maps API</li>
+            <li>Trip to Carbon API</li>
+          </ul>
+        </div>
+        <div className="nine wide column">
+          <h3>Calculations</h3>
+          <p>This app uses calculates the carbon data based around the average stuff to go in here as we want to explain what the data is. Define the underlying principles that drive decisions and strategy for your design language. A better understanding of usage can aid in prioritizing future efforts. Out of scope. All hands on deck marketing computer development html roi feedback team website today shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud , but thinking outside the box proceduralize, for work. </p>
+        </div>
+      </div>
+      <div className="ui height grid tagline">
+          <p>Created with love Project GreenPrint <span>{(new Date().getFullYear())}</span>
+          </p>
+      </div>
     </div>
     
   )
