e14fc3c5a1a6458789201d3b2617c80610f79408
Andy Jones <andytjones@icloud.com>
2020-01-13 13:00:18 +0000

    Styling - Footer styled.

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -6,6 +6,7 @@ function Welcome(props) {
       <h1>GreenPrint</h1>
       <p>Hello {props.name}!</p>
       <p>Small introduction text regarding the service and what to do here. This can say what our app does. Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+      <p>Small introduction text regarding the service and what to do here. This can say what our app does. Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
     </div>
   )
 }
