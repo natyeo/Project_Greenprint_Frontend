a810ecfb13509d872e708fab37f2a73e09d0f52f
Natalie <49326857+natyeo@users.noreply.github.com>
2020-01-13 17:15:47 +0000

    Add scaffold for bar chart

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
+import './profile.css';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -12,11 +13,47 @@ class Profile extends React.Component {
   }
 
   render() {
-    return fetch('https://cors-anywhere-project-greenprint.herokuapp.com/travel/record/user/:user_id')
+    const userId =
+    fetch(`https://cors-anywhere.herokuapp.com/https://project-greenprint-backend.herokuapp.com/travel/record/user/:${userId}`,
+      {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(data => data.json())
+      .then(body => {
+        this.setState(body.data);
+      })
 
+      const data = [
+        { name: 'Page A', uv: 4000 },
+        { name: 'Page B', uv: 3000 },
+        { name: 'Page C', uv: 2000 },
+        { name: 'Page D', uv: 2780 },
+        { name: 'Page E', uv: 1890 },
+      ];
+
+      return (
+        <BarChart width={600} height={300} data={data}
+        margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <Tooltip/>
+        <Legend />
+        <Bar dataKey="pv" fill="#8884d8" />
+        </BarChart>
+      );
+
+      ReactDOM.render(
+        <SimpleBarChart />,
+        document.getElementById('container')
+      );
+    }
   }
 
-}
 
-
-export default Profile;
+  export default Profile;
