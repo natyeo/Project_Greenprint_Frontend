667003e8071105ebe6771385bda3386c2846e5fc
Natalie <49326857+natyeo@users.noreply.github.com>
2020-01-15 11:52:54 +0000

    Bar chart showing total carbon by mode of transport and placeholder for offsetting tips

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -28,17 +28,17 @@ class Profile extends React.Component {
       })
     }
 
-    distanceByMode(cycling, driving, transit, walking) {
+    distanceByMode(cyclingDistance, drivingDistance, transitDistance, walkingDistance) {
       return this.state.journeys.forEach(
         function(elem) {
           if(elem.mode === "bicycling"){
-            cycling.value += elem.distance
+            cyclingDistance.value += elem.distance
           } else if (elem.mode === "driving") {
-            driving.value += elem.distance
+            drivingDistance.value += elem.distance
           } else if (elem.mode === "transit") {
-            transit.value += elem.distance
+            transitDistance.value += elem.distance
           } else if (elem.mode === "walking") {
-            walking.value += elem.distance
+            walkingDistance.value += elem.distance
           }
         }
       )
@@ -52,27 +52,45 @@ class Profile extends React.Component {
       }, []);
     }
 
+    carbonByMode(transitCarbon, drivingCarbon) {
+      return this.state.journeys.forEach(
+        function(elem) {
+          if(elem.mode === "transit"){
+            transitCarbon.carbon += elem.carbon
+        } else if (elem.mode === "driving"){
+            drivingCarbon.carbon += elem.carbon
+        }
+      })
+    }
+
     render() {
       if (!this.state.journeys) {
         return <div />
       } else {
+
+        const cyclingDistance = {name: 'Cycling', value: 0};
+        const drivingDistance = {name: 'Driving', value: 0};
+        const transitDistance = {name: 'Public transport', value: 0};
+        const walkingDistance = {name: 'Walking', value: 0};
+
+        const transitCarbon = {name: 'Public transport', carbon:0};
+        const drivingCarbon = {name: 'Driving', carbon: 0};
+
         const barData = this.carbonPerJourney(this.state.journeys);
-        const cycling = {name: 'Cycling', value: 0};
-        const driving = {name: 'Driving', value: 0};
-        const transit = {name: 'Public transport', value: 0};
-        const walking = {name: 'Walking', value: 0};
 
-        this.distanceByMode(cycling, driving, transit, walking);
+        this.distanceByMode(cyclingDistance, drivingDistance, transitDistance, walkingDistance);
+        const pieData = [cyclingDistance, drivingDistance, transitDistance, walkingDistance];
 
-        const pieData = [cycling, driving, transit, walking]
+        this.carbonByMode(transitCarbon, drivingCarbon);
+        const verticalChartData = [transitCarbon, drivingCarbon]
 
         return (
           <div>
           <h1>My Journeys Dashboard</h1>
             <div id="barchart">
-            <h2>Carbon per journey (kg)</h2>
+            <h2>Carbon(kg) per journey taken by rail or car</h2>
               <BarChart width={600} height={300} data={barData}
-                margin={{top: 5, right: 10, left: 100, bottom: 5}}>
+                margin={{top: 10, right: 10, left: 100, bottom: 30}}>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <XAxis dataKey="name"/>
                 <YAxis />
@@ -82,11 +100,29 @@ class Profile extends React.Component {
               </BarChart>
             </div>
             <div id="piechart">
-              <h2>Distance travelled by mode of transport (miles)</h2>
-              <PieChart width={800} height={400}>
+              <h2>Distance(miles) travelled by mode of transport</h2>
+              <PieChart width={800} height={300}>
                 <Pie isAnimationActive={true} data={pieData} cx={400} cy={150} outerRadius={100} fill="#8884d8" label/>
                 <Tooltip/>
               </PieChart>
+            </div>
+            <div id="sidebar">
+              <h2>Total carbon(kg) by mode of transport</h2>
+              <BarChart layout="vertical" width={600} height={300} data={verticalChartData}
+                margin={{top: 10, right: 30, left: 100, bottom: 30}}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis type="number"/>
+                <YAxis type="category" dataKey="name"/>
+                <Tooltip/>
+                <Legend />
+                <Bar dataKey="carbon" fill="#8884d8" />
+              </BarChart>
+            </div>
+            <div id="recommendations">
+              <h2>Some things you can do to offset your carbon emmissions..</h2>
+              <p> Invest in carbon offsetting projects like this one:
+                <a href="https://www.carbonfootprint.com/carbonoffsetprojects.html"> carbonfootprint.com</a>
+               </p>
             </div>
           </div>
         );
