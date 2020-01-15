99b9c22bed6757d89c610128dd847319999d4e1b
SevenSecrets <freddieshughes@googlemail.com>
2020-01-15 15:46:20 +0000

    Feature - Can search, display, and save flights

--- src/components/journey.jsx
+++ src/components/journey.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import JourneyOptions from "./journeyoptions";
+import JourneyOptionsFlying from "./flightoptions";
 import "./journey.css";
 import Modal from "./Modal/Modal";
 import Welcome from "./welcome.jsx";
@@ -13,7 +14,9 @@ class JourneyForm extends React.Component {
       to: "",
       options: false,
       loading: false,
-      loading_error: false
+      loading_error: false,
+      flying_from: "",
+      flying_to: ""
     };
   }
 
@@ -28,15 +31,13 @@ class JourneyForm extends React.Component {
 
   apiCall() {
     const data = { from: this.state.from, to: this.state.to };
-    // remember to change the route below for production / TEST
     return fetch(
       "https://cors-anywhere.herokuapp.com/https://project-greenprint-backend.herokuapp.com/",
       {
         method: "POST",
         headers: {
           Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
+          "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
       }
@@ -51,11 +52,41 @@ class JourneyForm extends React.Component {
     })
   }
 
+  apiCallFlying() {
+    const data = { from: this.state.flying_from, to: this.state.flying_to };
+    return fetch(
+      "https://cors-anywhere.herokuapp.com/https://project-greenprint-backend.herokuapp.com/flights",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+    )
+    .then(data => data.json())
+    .then(body => {
+      this.setState({
+        options: body,
+        loading: false
+      });
+      console.log(body)
+      return body;
+    })
+  }
+
   handleSubmit = event => {
     this.apiCall();
     this.setState({ loading: true });
     event.preventDefault();
   };
+
+  handleSubmitFlying = event => {
+    this.apiCallFlying();
+    this.setState({ loading: true });
+    event.preventDefault();
+  }
 
   journeyOptionsList() {
     const startPoint = this.state.from;
@@ -75,63 +106,108 @@ class JourneyForm extends React.Component {
     });
   }
 
-  render() {
+  journeyOptionsListFlying() {
+    const startPoint = this.state.flying_from;
+    const endPoint = this.state.flying_to;
+    const object = this.state.options
+    if (this.state.options.error) {
+      return(<BadRequestError error={this.state.options.error} description={this.state.options.description}/>)
+    }
     return (
-      <div>
-      <div>
-      <Welcome name="there" />
-      </div>
-
-      <div className="grouping">
-      <h3>Journey Calculation</h3>
-      <form onSubmit={this.handleSubmit} className="ui form">
-      <div>
-      <label>
-      <span className="bold">FROM</span>
-      <input
-      name="from"
-      type="text"
-      value={this.state.from}
-      onChange={this.handleChange}
+      <JourneyOptionsFlying
+      results={object}
+      from={startPoint}
+      to={endPoint}
       />
-      </label>
-      </div>
-      <div>
-      <label>
-      <span className="bold">TO</span>
-      <input
-      name="to"
-      type="text"
-      value={this.state.to}
-      onChange={this.handleChange}
-      />
-      </label>
-      </div>
-      <div>
-      <input className="ui button" type="submit" value="Submit" />
-      </div>
-      </form>
-      </div>
-
-      {this.state.options ? (
-        <div className="grouping_results">
-          <h2>Your travel results</h2>
-          <div className="grouping_table">
-          <table
-          className="ui celled striped table"
-          style={{ display: 'flex',  justifyContent:'center', alignItems:'center', textAlign: "center" }}
-          >
-          <tbody>{this.journeyOptionsList()}</tbody>
-          </table>
-        </div>
-        </div>
-      ) : (
-        <> </>
-      )}
-      {this.state.loading ? <Modal /> : <> </>}
-      </div>
     );
-  }
+}
+
+render() {
+  return (
+    <div>
+    <div>
+    <Welcome name="there" />
+    </div>
+
+    <h2>Carbon Calculator</h2>
+    <div className="grouping">
+    <h3>Ground Transport</h3>
+    <form onSubmit={this.handleSubmit} className="ui form">
+    <div>
+    <label>
+    <span className="bold">FROM</span>
+    <input
+    name="from"
+    type="text"
+    value={this.state.from}
+    onChange={this.handleChange}
+    />
+    </label>
+    </div>
+    <div>
+    <label>
+    <span className="bold">TO</span>
+    <input
+    name="to"
+    type="text"
+    value={this.state.to}
+    onChange={this.handleChange}
+    />
+    </label>
+    </div>
+    <div>
+    <input className="ui button" type="submit" value="Submit" />
+    </div>
+    </form>
+
+    <h3>Flights</h3>
+    <form onSubmit={this.handleSubmitFlying} className="ui form">
+    <div>
+    <label>
+    <span className="bold">Origin Aiport</span>
+    <input
+    name="flying_from"
+    type="text"
+    value={this.state.flying_from}
+    onChange={this.handleChange}
+    />
+    </label>
+    </div>
+    <div>
+    <label>
+    <span className="bold">Destination Airport</span>
+    <input
+    name="flying_to"
+    type="text"
+    value={this.state.flying_to}
+    onChange={this.handleChange}
+    />
+    </label>
+    </div>
+    <div>
+    <input className="ui button" type="submit" value="Submit" />
+    </div>
+    </form>
+    </div>
+
+    {this.state.options ? (
+      <div className="grouping_results">
+      <h2>Your travel results</h2>
+      <div className="grouping_table">
+      {this.state.options.mode === "flying" ? (
+        this.journeyOptionsListFlying()
+      ) : (
+        this.journeyOptionsList()
+      )}
+      </div>
+      </div>
+    ) : (
+      <> </>
+    )}
+    {this.state.loading ? <Modal /> : <> </>}
+    </div>
+  );
+}
 }
 
 export default JourneyForm;
