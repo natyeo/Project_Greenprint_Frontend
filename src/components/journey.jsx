9b3026b43d5c25907a220014461a1fefab6c1bb8
Natalie <49326857+natyeo@users.noreply.github.com>
2020-01-16 09:48:55 +0000

    Add required on From and To fields
    Co-Authored-By: elfiyang16 <elfiyang16@gmail.com>

--- src/components/journey.jsx
+++ src/components/journey.jsx
@@ -140,6 +140,7 @@ render() {
     type="text"
     value={this.state.from}
     onChange={this.handleChange}
+    required
     />
     </label>
     </div>
@@ -151,6 +152,7 @@ render() {
     type="text"
     value={this.state.to}
     onChange={this.handleChange}
+    required
     />
     </label>
     </div>
